4a4895d7662fac3784e5835d32b159d59af75328
Henry KIM <seedtree.henry@gmail.com>
2018-07-04 17:36:05 +1000

    scaffolding classroom

---
+++ app/views/classrooms/_classroom.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! classroom, :id, :name, :user_id, :created_at, :updated_at
+json.url classroom_url(classroom, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -26,6 +26,7 @@
 #                          DELETE /users/:id(.:format)           users#destroy
 
 Rails.application.routes.draw do
+  resources :classrooms
   devise_for :users, controllers: { :registrations => :registrations }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, :controller => 'users'
